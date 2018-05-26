d12f175821ca240adfdce7d571e312b26628514a
viniman <vinicius.carlos@live.com>
2018-05-26 16:00:08 -0300

    Implementacao da Arvore B

---
+++ Headers/BTree.h
@@ -0,0 +1,36 @@
+//
+// Created by viniman on 5/26/18.
+//
+
+#ifndef TRABALHO_ED2_2_BTREE_H
+#define TRABALHO_ED2_2_BTREE_H
+
+
+#include <iostream>
+#include "NodeB.h"
+
+template <class T> class BTree {
+private:
+    NodeB<T>* raiz;
+    void deletaArvore(NodeB<T>* no);
+
+public:
+    BTree<T>();
+    ~BTree<T>();
+    void insere(T valor);
+    bool find(T valor);
+    NodeB<T>* insereNo(NodeB<T> *no, T valor);
+    void insereRecursivo(NodeB<T> *no, T valor);
+    int split(NodeB<T> *n, NodeB<T> **novoNo);
+    NodeB<T>* find(NodeB<T> *n, T valor);
+    NodeB<T>** getRaiz();
+    void imprime();
+    void imprimePorNivel(NodeB<T> *p, int nivel);
+    void inOrder();
+    void inOrder(NodeB<T> *p);
+
+
+};
+
+
+#endif //TRABALHO_ED2_2_BTREE_H
